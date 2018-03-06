5a6af5e71a7ef3963d76ded882bb903b4b7e3c33
HungPCV <shoknshok@gmail.com>
2018-03-06 17:05:21 +0700

    Add exception handler for CameraCapture avoid "core dump" execption from OpenCV

--- Units/CameraHandler.cpp
+++ Units/CameraHandler.cpp
@@ -17,25 +17,25 @@ Xử lý tác vụ của camera
 namespace fs = std::experimental::filesystem;
 ///************** FUCNTION **************///
 // Description: Contructor
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-CameraHandler::CameraHandler(){
-	m_pCamData = NULL;
-
+CameraHandler::CameraHandler()
+{
+    m_pCamData = NULL;
 }
 ///************** FUCNTION **************///
 // Description: Decontructor
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-CameraHandler::~CameraHandler(){
-
+CameraHandler::~CameraHandler()
+{
 }
 
 // Description: Chụp hình từ camera
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
 // void Camera::CaptureImage(){
 //     if( !isOpened() ){
@@ -57,79 +57,104 @@ CameraHandler::~CameraHandler(){
 //     g_pSmartBellData.m_bAlarmFlag = false;
 // }
 // Description: Chụp hình từ camera
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-void CameraHandler::SetCamdata(CameraData* pCamData){
+void CameraHandler::SetCamdata(CameraData *pCamData)
+{
     m_pCamData = pCamData;
 }
 
-
-void CameraHandler::SaveImage(){
-    std::string img_directory = IMAGE_DIR + m_pCamData->GetCamID();// + "/";
-    //std::cout << "Write Image to: " << test << std::endl;
-    if(!fs::exists(img_directory)) fs::create_directory(img_directory);
-    cv::imwrite(img_directory + "/" + m_pCamData->GetPath(),m_pCamData->GetImage());
+void CameraHandler::SaveImage()
+{
+    std::string img_directory = IMAGE_DIR + m_pCamData->GetCamID(); // + "/";
+    std::cout << "Write Image to: " << img_directory << std::endl;
+    if (!fs::exists(img_directory))
+        fs::create_directory(img_directory);
+    cv::imwrite(img_directory + "/" + m_pCamData->GetPath(), m_pCamData->GetImage());
 }
 
 // Description: Chụp hình từ camera
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-void *CameraHandler::CaptureThread (void *args){
+void *CameraHandler::CaptureThread(void *args)
+{
     CaptureResult returncode = CaptureImage();
 }
 // Description: Chụp hình từ camera
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-CaptureResult CameraHandler::CaptureImage(){
-     
-	if(!m_pCamData->GetMainURL().empty()){
-        cv::VideoCapture cap(m_pCamData->GetMainURL(), cv::VideoCaptureAPIs::CAP_GSTREAMER);
-        if( cap.isOpened() )
+CaptureResult CameraHandler::CaptureImage()
+{
+
+    if (!m_pCamData->GetMainURL().empty())
+    {
+        try
         {
-            cv::Mat frame;
-            cap >> frame;
-            if( !frame.empty() ) {
-                m_pCamData->SetImage(frame);
-                SaveImage();//Lưu hình
-                return CAM_SUCCESS;
+            cv::VideoCapture cap(m_pCamData->GetMainURL(), cv::VideoCaptureAPIs::CAP_GSTREAMER);
+            if (cap.isOpened())
+            {
+                cv::Mat frame;
+                cap >> frame;
+                if (!frame.empty())
+                {
+                    m_pCamData->SetImage(frame);
+                    SaveImage(); //Lưu hình
+                    return CAM_SUCCESS;
+                }
             }
+        }
+        catch (cv::Exception &e)
+        {
+            const char *err_msg = e.what();
+            std::cout << "exception caught: " << err_msg << std::endl;
         }
     }
-    if(!m_pCamData->GetSubURL().empty()){
-        cv::VideoCapture cap(m_pCamData->GetSubURL(), cv::VideoCaptureAPIs::CAP_GSTREAMER);
-        if( cap.isOpened() )
+    if (!m_pCamData->GetSubURL().empty())
+    {
+        try
         {
-            cv::Mat frame;
-            cap >> frame;
-            if( !frame.empty() ) {
-                m_pCamData->SetImage(frame);
-                SaveImage();//Lưu hình
-                return CAM_SUCCESS;
+            cv::VideoCapture cap(m_pCamData->GetSubURL(), cv::VideoCaptureAPIs::CAP_GSTREAMER);
+            if (cap.isOpened())
+            {
+                cv::Mat frame;
+                cap >> frame;
+                if (!frame.empty())
+                {
+                    m_pCamData->SetImage(frame);
+                    SaveImage(); //Lưu hình
+                    return CAM_SUCCESS;
+                }
+                return CAM_ERROR_EMPTY_FRAME;
             }
-            return CAM_ERROR_EMPTY_FRAME;
+            return CAM_ERROR_CLOSE;
         }
-        return CAM_ERROR_CLOSE;
+        catch (cv::Exception &e)
+        {
+            const char *err_msg = e.what();
+            std::cout << "exception caught: " << err_msg << std::endl;
+        }
     }
     return CAM_ERROR_INVALID_URL;
 }
 
-
 // Description: Chụp hình từ camera
-// Parameters: 
-//		
+// Parameters:
+//
 // Return: None
-int CameraHandler::CaptureImage(CameraData* pCamData){
+int CameraHandler::CaptureImage(CameraData *pCamData)
+{
     SetCamdata(pCamData);
     // Chụp hình
     //thread_create(&m_thrCapture,NULL,&CameraHandler::CaptureThread,NULL); // Đa luồng
     CaptureResult returncode = CaptureImage();
-    if(returncode)
-    {   std::cout << "Capture return: " << returncode << std::endl;
+    if (returncode)
+    {
+        std::cout << "Capture return: " << returncode << std::endl;
         return returncode;
-    };// Đơn luồng
+    }; // Đơn luồng
     // Lưu hình
     //SaveImage();
     std::cout << "Capture success! " << std::endl;
